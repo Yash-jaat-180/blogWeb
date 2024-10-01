5b3183e17c6b6dbaf24339f83ec93910b4faa5a0
Yash <yashsaharan18024@gmail.com>
2024-10-01 21:58:04 +0530

    header

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Header from "./header/header.jsx";
+import Header from './header/Header.jsx'
 import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
