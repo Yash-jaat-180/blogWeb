4dc334b04fa66a3ac4f00954d8638e01920a0070
Yash <yashsaharan18024@gmail.com>
2024-10-01 21:19:06 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./header/header";
+import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./RTE";
+import Select from "./Select"
+import Button from "./Button";
+import SignUp from "./Signup";
+import Login from "./Login";
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard';
+import AuthLayout from './AuthLayout'
+import Input from "./Input";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo, 
+    LogoutBtn,
+    RTE,
+    Select,
+    Button,
+    SignUp,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Input,
+    
+}
