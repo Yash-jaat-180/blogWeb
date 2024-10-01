8491c3b5c356b2de5dd70a894dda2cd84d44f0ff
Yash <yashsaharan18024@gmail.com>
2024-10-01 21:50:58 +0530

    header

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
-import Header from "./header/header";
+import Header from "./header/header.jsx";
 import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
 import RTE from "./RTE";
-import Select from "./Select"
+import Select from "./Select.jsx"
 import Button from "./Button";
 import SignUp from "./Signup";
 import Login from "./Login";
@@ -12,7 +12,7 @@ import PostForm from './post-form/PostForm'
 import PostCard from './PostCard';
 import AuthLayout from './AuthLayout'
 import Input from "./Input";
-export {
+export  {
     Header,
     Footer,
     Container,
