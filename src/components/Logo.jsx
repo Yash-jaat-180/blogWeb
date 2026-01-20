4d79cf99e9e06f1e9a781c178cb0d846f912e9fe
Yash <yashsaharan180@gmail.com>
2026-01-20 23:17:51 +0530

    fix: restore img tag class for consistent styling in Logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import blog from '../../public/blog.jpeg'
 function Logo(width = '100px') {
   return (
     <div>
-      <img
+      <img className="w-16 h-16 rounded-lg object-contain"
         src={blog}
         alt="Logo"
         style={{ width: "120px", height: "120px" }}
